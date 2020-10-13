c49544b1f6ba327e46af522588fbeae18e7f5af8
Luis1505 <luis.montanez-vazquez@patelhs.org>
2020-10-13 13:04:34 +0000

    I added my scanner and I also added the if/else statement.

--- Main.java
+++ Main.java
@@ -1,5 +1,28 @@
+  import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+    System.out.println("Welcome to the truck race!, will you Race or Spectate!");
+    // intro to scanner 
+
+    Scanner scan = new Scanner(System.in);
+  
+    System.out.println("1 for race. 0 for Spectate");
+    int confirmation = scan.nextInt();// The if/else statement
+
+    if (confirmation == 1){
+    System.out.println("Have a nice race !"); 
+    }else if(confirmation == 0 ){
+    System.out.println("Have Fun Watching!");
+   }
+
+ }
 }
+
+
+  
+
+
+
+    
+  
