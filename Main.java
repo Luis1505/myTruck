e35143f38675bd233b2c47d3cc45ab9d1ec2f06a
Luis1505 <luis.montanez-vazquez@patelhs.org>
2020-10-14 17:59:37 +0000

    i added the if/else statement

--- Main.java
+++ Main.java
@@ -3,18 +3,33 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Welcome to the truck race!, will you Race or Spectate!");
-    // intro to scanner 
+    // intro to scanner and user input
 
     Scanner scan = new Scanner(System.in);
   
     System.out.println("1 for race. 0 for Spectate");
     int confirmation = scan.nextInt();// The if/else statement
 
-    if (confirmation == 1){
-    System.out.println("Have a nice race !"); 
+    if (confirmation == 1){ // Then the color of the truck
+    System.out.println("What color truck do you want?");
+    System.out.println("We have 1-purple, 2-blue, 3-red and 4-green");
+    int color = scan.nextInt();
+      if (color == 2){
+       System.out.println("You lose, You got 2nd place");
+
+      }else if(color == 1){
+         System.out.println("You Win! You got 1st place");
+      }else if(color == 3){
+          System.out.println("You lose, You came in dead last.");
+      }else if(color == 4){
+          System.out.println("You Lose,You came in 3rd place.");
+        }  
+    
     }else if(confirmation == 0 ){
     System.out.println("Have Fun Watching!");
+    System.out,println("Would like food ?");
    }
+    
 
  }
 }
